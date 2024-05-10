6039f562d6f14c5c3f2af6d3003ad7bdc4b973b9
perryjre <perryjre@github.com>
2024-05-10 15:26:35 -0400

    fixes errors

--- service/routes.py
+++ service/routes.py
@@ -20,7 +20,7 @@ Product Store Service with UI
 """
 from flask import jsonify, request, abort
 from flask import url_for  # noqa: F401 pylint: disable=unused-import
-from service.models import Product
+from service.models import Product, Category
 from service.common import status  # HTTP Status Codes
 from . import app
 
@@ -93,15 +93,15 @@ def create_products():
     location_url = "/"  # delete once READ is implemented
     return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
-
 ######################################################################
 # L I S T   A L L   P R O D U C T S
 ######################################################################
 
+
 @app.route("/products", methods=["GET"])
 def list_products():
     """Returns a list of Products"""
-    app.logger.info("Request to list Products...")
+    app.logger.info("list products")
 
     products = []
     name = request.args.get("name")
@@ -109,20 +109,20 @@ def list_products():
     available = request.args.get("available")
 
     if name:
-        app.logger.info("Find by name: %s", name)
+        app.logger.info(f"list by {name}")
         products = Product.find_by_name(name)
     elif category:
-        app.logger.info("Find by category: %s", category)
+        app.logger.info(f"list by {category}")
         # create enum from string
-        category_value = getattr(category, category.upper())
+        category_value = getattr(Category, category.upper())
         products = Product.find_by_category(category_value)
     elif available:
-        app.logger.info("Find by available: %s", available)
+        app.logger.info(f"list by {available}")
         # create bool from string
         available_value = available.lower() in ["true", "yes", "1"]
         products = Product.find_by_availability(available_value)
     else:
-        app.logger.info("Find all")
+        app.logger.info("list all")
         products = Product.all()
 
     results = [product.serialize() for product in products]
